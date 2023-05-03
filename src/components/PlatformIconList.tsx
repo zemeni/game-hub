d93738aa12d25cbae0837916f28a9c1fba2af654
babu <neupanebabu828@gmail.com>
2023-05-03 23:36:23 +0930

    get optimized images

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -33,7 +33,7 @@ const PlatformIconList = ({platforms}: Props) => {
     return (
         <HStack marginY={1}>
             {/*1 = 4px*/}
-            {platforms.map(platform => <Icon as={iconMap[platform.slug]} color='gray.500'/>)}
+            {platforms.map(platform => <Icon key={platform.id} as={iconMap[platform.slug]} color='gray.500'/>)}
         </HStack>
     );
 };
